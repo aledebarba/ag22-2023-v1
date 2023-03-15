6e0eabb170c5bc4e886ff407aaae7e3ffa0241e2
Alexandre CMC Souza <ale@alemacedo.com>
2023-03-15 18:48:35 -0300

    package.json script adjustments

--- theme-frontend/build/_app.asset.php
+++ theme-frontend/build/_app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch'), 'version' => 'f96c992c56667b6fa814');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch'), 'version' => 'e52cd0a1f4fd90eeb464');
