f7f5348f461b7cba9612edb353880fb62e7120b9
Alexandre CMC Souza <ale@alemacedo.com>
2023-03-18 17:00:22 -0300

    Adicionei Smooth Scroller

--- theme-frontend/build/_app.asset.php
+++ theme-frontend/build/_app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch'), 'version' => '73835e2b4b29d4be58fb');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch'), 'version' => '1ae18deed80b22b2662d');
