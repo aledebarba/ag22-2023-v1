06b7455dbdafceb9d1cd7c6c2434e104bd153971
Alexandre CMC Souza <ale@alemacedo.com>
2023-03-19 17:23:21 -0300

    Layout inicial de cases

--- theme-frontend/build/_app.asset.php
+++ theme-frontend/build/_app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch'), 'version' => '8fb02832ad9619734909');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch'), 'version' => 'af0ea346495fd9fbed63');
