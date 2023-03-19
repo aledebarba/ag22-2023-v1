49b579fb73a356570b248e886858e4b310326b1a
Alexandre CMC Souza <ale@alemacedo.com>
2023-03-19 16:02:34 -0300

    Novo tipo de dado - Projetos
    Em andamento

--- theme-frontend/build/_app.asset.php
+++ theme-frontend/build/_app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch'), 'version' => '2f3e3dd0daf2ec0768cf');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch'), 'version' => 'c16af285b224a966abb5');
--- theme-frontend/src/components/Cases.jsx
+++ theme-frontend/src/components/Cases.jsx
@@ -1,5 +1,6 @@
 import { ContainerFluidH } from './containers';
 import { H2Dash } from './headings';
+import { Buttonx } from './button';
 import tw from 'twin.macro';
 import { useEffect, useState } from 'react';
 import apiFetch from '@wordpress/api-fetch';
@@ -17,7 +18,9 @@ export const Cases = () => {
 	
 	
 	return <section className="Cases" tw="py-28 relative overflow-visible">
+				
 				<div tw="absolute border-[6rem] border-primary rounded-[999rem] w-[40vw] h-[40vw] top-[-20vw] left-[-18vw]" />
+				
 				<ContainerFluidH tw="relative">
 					<H2Dash>Cases</H2Dash>
 					<div tw="grid grid-cols-3 gap-8 mt-16">
@@ -28,16 +31,46 @@ export const Cases = () => {
 }
 
 const CaseCard = ({ item, index }) => {
-		return <div css={[
+		
+	
+	return <div 
+		css={[
+			`position: relative`,
 			index === 0 && tw`col-span-2`,
 			index !== 0 && tw`col-auto`,
-			`
-			width: auto;
-			height: 40vh;
-			background-image: url(${item.data.image});
-			background-size: cover;
-			background-position: center;
-			background-repeat: no-repeat;
-			border-radius: 1rem;
-		`]} />
+		]}		
+	>
+		<img 
+			tw="w-full h-full object-cover rounded-2xl"
+			src={item.image}
+		/>
+		<div 
+			tw="absolute bottom-0 left-0 w-full h-4"
+		>
+			<h6>
+				{item.title}
+			</h6>
+			<p>
+				{item.description}
+			</p>
+			<Buttonx>
+				<a href="#">Ir para o projeto</a>
+			</Buttonx>
+		</div>
+	</div>
+		
+		
+		// <div css={[
+		// 	index === 0 && tw`col-span-2`,
+		// 	index !== 0 && tw`col-auto`,
+		// 	`
+		// 		width: auto;
+		// 		height: 40vh;
+		// 		background-image: url(${item.data.image});
+		// 		background-size: cover;
+		// 		background-position: center;
+		// 		background-repeat: no-repeat;
+		// 		border-radius: 1rem;
+
+		// `]} />
 }
