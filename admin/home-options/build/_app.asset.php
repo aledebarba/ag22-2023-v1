8d332453954170d8319f53382277a4f5becf3236
Alexandre CMC Souza <ale@alemacedo.com>
2023-03-21 11:12:33 -0300

    Tela de opções
    60%

--- admin/home-options/build/_app.asset.php
+++ admin/home-options/build/_app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'f93ac8b3ed1d87028b4f');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '2abf6a523b9607de3549');
--- admin/home-options/src/index.jsx
+++ admin/home-options/src/index.jsx
@@ -1,7 +1,6 @@
 import tw from 'twin.macro'
 import { Icon } from '@iconify/react'
 import { useState } from 'react'
-import Toggle from 'react-toggle'
 
 const App = () => {
   const [options, setOptions] = useState({
@@ -12,7 +11,16 @@ const App = () => {
     heroImage: '',
     introImage: '',
     aboutImage: '',
-    aboutMovie: ''
+    aboutMovie: '',
+    favicon: '',
+    description: '',
+    tags: '',
+    ogTitle: '',
+    ogDescription: '',
+    ogImage: '',
+    twitterCardText: '',
+    twitterCardTitle: '',
+    twitterCardAuthor: '',
   })
 
   return (
@@ -22,35 +30,126 @@ const App = () => {
         Administração do site e opções globais
       </h1>
       <Section title='Opções de disponibilidade'>
-       <div className="option">
-        <Toggle
-            id='online-offline'
-            defaultChecked={options.online}
-            aria-labelledby='biscuit-label'
-            onChange={e => setOptions({ ...options, online: e.target.checked })}
-            />
-        <span id='online-offline-label'>O site está online.</span>
-       </div>
-       <div className="option">
+        <Option label="Online / Offline">
+            switcher
+        </Option>
+        <Option label="Favicon">
+            favicon
+        </Option>
+       <Option label="Título do site" width="100%">
             <input 
                 type="text" 
                 placeholder="Título do site"
                 value={options.title}
                 onChange={e => setOptions({ ...options, title: e.target.value })}
-                tw="border border-slate-200 p-2 rounded-lg bg-white my-4 shadow-lg w-2/3"
+                tw="border border-slate-200 p-2 rounded-lg bg-white shadow-lg w-full"
             />
-         </div>
+         </Option>
         
       </Section>
-      <Section title='Opções de contatos e redes sociais'>
-      </Section>
       <Section title='Opções da landing page'>
+            <Option label="Ordem das redes sociais na seção contatos" width="100%">
+                DragList com switcher de ligar / desligar
+            </Option>
+            <Option label="Ordem das redes sociais no rodapé" width="100%">
+                DragList com switcher de ligar / desligar
+            </Option>
+            <Option label="Ordem dos cases na seção cases" width="100%">
+                DragList com switcher de ligar / desligar
+            </Option>
+            <Option label="Imagem de fundo da seção de introdução" width="100%">
+                Select Image 
+            </Option>            
       </Section>
       <Section title='Opções da página Sobre Nós'>
-      </Section>
-      <Section title='Opções da página Sobre Nós'>
-      </Section>
+        <Option label="Video da seção sobre nós" width="100%">
+            Select Video / URL
+        </Option>
+      </Section>     
+
+
       <Section title='Opções de SEO'>
+
+
+        <Option label="Descrição do site" width="40%">
+            <textarea
+                placeholder="Descrição do site"
+                value={options.description}
+                onChange={e => setOptions({ ...options, description: e.target.value })}
+                lines={5}
+                tw="border border-slate-200 p-2 rounded-lg bg-white shadow-lg w-full h-[10ch]"
+            />
+        </Option>
+
+
+        <Option label="Tags do site" width="40%">
+            <input 
+                type="text" 
+                placeholder="tags, separadas, por, vírgula"
+                value={options.tags}
+                onChange={e => setOptions({ ...options, tags: e.target.value })}
+                tw="border border-slate-200 p-2 rounded-lg bg-white shadow-lg w-full"
+            />
+        </Option>
+
+        <Line/>
+        
+        <Option label="Open Graph: Titulo" width="100%">
+            <input 
+                type="text" 
+                placeholder="Titulo de cards compartilhados no Facebook"
+                value={options.ogTitle}
+                onChange={e => setOptions({ ...options, ogTitle: e.target.value })}
+                tw="border border-slate-200 p-2 rounded-lg bg-white shadow-lg w-full"
+            />
+        </Option>
+
+        <Option label="Open Graph: Descrição" width="40%">
+            <textarea
+                placeholder="Descrição de cards compartilhados no Facebook"
+                value={options.ogDescription}
+                onChange={e => setOptions({ ...options, ogDescription: e.target.value })}
+                lines={5}
+                tw="border border-slate-200 p-2 rounded-lg bg-white shadow-lg w-full h-[10ch]"
+            />
+        </Option>
+
+        <Option label="Open Graph: Imagem" width="40%">
+            Select Image
+        </Option>
+
+        <Line/>
+
+        <Option label="Twitter Card: Título" width="100%">
+            <input
+                type="text"
+                placeholder="Titulo de cards compartilhados no Twitter"
+                value={options.twitterCardTitle}
+                onChange={e => setOptions({ ...options, twitterCardTitle: e.target.value })}
+                tw="border border-slate-200 p-2 rounded-lg bg-white shadow-lg w-full"
+            />
+        </Option>
+
+        <Option label="Twitter Card: Texto" width="40%">
+            <textarea
+                placeholder="Descrição de cards compartilhados no Twitter"
+                value={options.twitterCardText}
+                onChange={e => setOptions({ ...options, twitterCardText: e.target.value })}
+                lines={5}
+                maxLength={250}
+                tw="border border-slate-200 p-2 rounded-lg bg-white shadow-lg w-full h-[10ch]"
+            />
+        </Option>
+
+        <Option label="Twitter Card: Autor" width="40%">
+            <input
+                type="text"
+                placeholder="Autor de cards compartilhados no Twitter"
+                value={options.twitterCardAuthor}
+                onChange={e => setOptions({ ...options, twitterCardAuthor: e.target.value })}
+                tw="border border-slate-200 p-2 rounded-lg bg-white shadow-lg w-full"
+            />
+        </Option>
 
       </Section>
 
@@ -62,11 +161,40 @@ export default App
 
 const Section = ({ title, icon = 'ant-design:control-twotone', children }) => {
   return (
-    <section tw='p-4 rounded-lg bg-gray-50 border border-4 border-white my-4 shadow-lg w-full sm:(w-2/3)'>
+    <section tw='p-4 rounded-lg bg-gray-50/50  my-4 shadow-lg w-full 
+    lg:(w-2/3) 2xl:(w-1/2) duration-300 hover:(bg-gray-50 duration-300)'
+        css="border: 1px solid white;">
       <h2 tw='text-slate-900 font-normal tracking-wider my-2 flex items-center gap-4'>
         <Icon icon={icon} tw="text-red-500"/> {title}
       </h2>
-      {children}
+      <div tw="flex gap-2 flex-wrap">
+        {children}
+      </div>
     </section>
   )
+}
+
+const Option = ({ label, width, children }) => {
+
+    return (
+        
+        <div css={[
+            tw`flex flex-col gap-2 p-2 bg-white rounded-md shadow-md mb-2`,
+            `min-width: 240px;`,
+            'max-width: 40vw;',
+            'height: 80%;',            
+            width ? `width: ${width};` : 'width: 20%;'
+            ]}>
+            <div tw="flex flex w-full items-center gap-1 mb-2">
+            <Icon icon="ph:gear-fill" tw="text-red-400"/><small tw="text-red-400 font-bold uppercase">{label}</small>
+            </div>
+            {children}
+        </div>
+    )
+}
+
+const Line = () => {
+    return (
+        <div tw="w-full h-[1px] bg-red-300 my-2"></div>
+    )
 }
