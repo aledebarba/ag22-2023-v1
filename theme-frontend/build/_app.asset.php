abe4a72c7089e011c07ca3dfb1710d1f34033ead
Alexandre CMC Souza <ale@alemacedo.com>
2023-04-05 09:20:37 -0300

    redo css

--- theme-frontend/build/_app.asset.php
+++ theme-frontend/build/_app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch'), 'version' => '640acfe7d824efb00283');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch'), 'version' => '0d679de9528a101dc5a0');
