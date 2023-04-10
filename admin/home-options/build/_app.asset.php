0feddf00f5406dcf5b524c487ad90f2fd84134ec
Alexandre CMC Souza <ale@alemacedo.com>
2023-04-10 12:03:56 -0300

    Atualização pontual de código fonte do tema

--- admin/home-options/build/_app.asset.php
+++ admin/home-options/build/_app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element'), 'version' => '1ab65e0d53bc4dd66a0e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element'), 'version' => '32e2aff9f06f62b0d774');
--- admin/home-options/src/components/cases-grid/cases-grid.js
+++ admin/home-options/src/components/cases-grid/cases-grid.js
@@ -157,7 +157,7 @@ export const CasesGrid = ( {options, cases, setOptions} ) => {
 
         
 const CellImage = ({caseData}) => { 
-    
+    console.log("cell image received case-data ", caseData )
     if( caseData === null || caseData.length === 0 ) {
         return <div tw="w-full h-full flex justify-center items-center">
             <Icon icon="carbon:no-image" width="42px" tw="text-slate-500"/>
