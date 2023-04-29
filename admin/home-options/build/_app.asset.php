3bd2eb01d75b3a3c7e49c2d66d364be510f15a86
Alexandre CMC Souza <ale@alemacedo.com>
2023-04-29 20:08:55 -0300

    Atualização pontual de código fonte do tema

--- admin/home-options/build/_app.asset.php
+++ admin/home-options/build/_app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element'), 'version' => '369ee2aa8cf97fbca3ab');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element'), 'version' => '7d076afd1ce7ea462f8a');
