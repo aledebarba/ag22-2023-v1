59635b7c14ad778b7944e5d0d8e5effdbd00dbe3
Alexandre CMC Souza <ale@alemacedo.com>
2023-05-04 18:13:33 -0300

    Criando novos arquivos de produção

--- admin/home-options/build/_app.asset.php
+++ admin/home-options/build/_app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element'), 'version' => 'd0af21326d9d03a87696');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element'), 'version' => 'b759767ab65e046268c4');
